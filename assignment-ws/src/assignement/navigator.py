66ea06fb7ecfe92879c76da6c6292e241a42c401
Robert Stevenson <78357805+robert-stevenson-1@users.noreply.github.com>
2023-12-04 21:33:52 +0000

    Init Assignment Workspace

---
+++ assignment-ws/src/assignement/navigator.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from assignement.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ assignment-ws/src/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'assignement'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robert',
+    maintainer_email='78357805+robert-stevenson-1@users.noreply.github.com',
+    description='Package containing robot programming assignment',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'navigator = assignement.navigator:main'
+        ],
+    },
+)
