b58fc4f49cbbe5f9be1a441dd4c4e7222247fbbe
Robert Stevenson <78357805+robert-stevenson-1@users.noreply.github.com>
2023-12-13 10:27:42 +0000

    Update assignement-script.sh

--- assignement-script.sh
+++ assignement-script.sh
@@ -9,13 +9,15 @@ fi
 
 
 # source the limo environment
-source "$mount_path/University/Lincoln/MSc/Robot-Programming/CMP9767_LIMO/limo_ros/install/setup.bash"
+source_limo_cmd="source $mount_path/University/Lincoln/MSc/Robot-Programming/CMP9767_LIMO/limo_ros/install/setup.bash"
+$source_limo_cmd
 
 # create a alias to kill these sessions
 alias kill_sim="tmux kill-session -t gazebo_sim"
 alias kill_rviz="tmux kill-session -t rviz2"
+alias kill_nav="tmux kill-session -t navigation"
 # kill all the sessions
-alias kill_all_tmux="kill_sim; kill_rviz"
+alias kill_all_tmux="kill_sim; kill_rviz; kill_nav"
 
 # check if the gazebo tmux session exists
 tmux has-session -t gazebo_sim 2>/dev/null
@@ -27,26 +29,43 @@ if [ $? != 0 ]; then
     echo "killed old gazebo_sim session"
     # kill the old sessions and restart a new one
     kill_sim
-    # cmd to launch the simulator world
     tmux new-session -s gazebo_sim -d
 fi
+# cmd to launch the simulator world
 sim_cmd="ros2 launch limo_gazebosim limo_gazebo_diff.launch.py world:=$mount_path/University/Lincoln/MSc/Robot-Programming/CMP9767_LIMO/limo_ros/src/limo_gazebosim/worlds/potholes_simple.world"
 tmux send-keys -t gazebo_sim "$sim_cmd" Enter
 
 # check if the rviz tmux session exists
-tmux has-session -t rviz2 2>/dev/null
+# tmux has-session -t rviz2 2>/dev/null
+# if [ $? != 0 ]; then
+#     # start the simulator in it's own tmux session if it doesn't exist
+#     echo "Starting rviz session"
+#     tmux new-session -s rviz2 -d
+#     else
+#     # kill the old sessions and restart a new one
+#     kill_rviz
+#     echo "killed old rviz2 session"
+#     tmux new-session -s rviz2 -d
+#     # start the rviz2 for the simulator
+# fi
+# rviz_file="$mount_path/University/Lincoln/MSc/Robot-Programming/CMP9767_LIMO/limo_ros/src/limo_gazebosim/rviz/urdf.rviz"
+# # rviz command
+# rviz_cmd="rviz2 -d $rviz_file"
+# tmux send-keys -t rviz2 "$rviz_cmd" Enter
+
+# check if we have a navigation tmux session
+tmux has-session -t navigation 2>/dev/null
 if [ $? != 0 ]; then
     # start the simulator in it's own tmux session if it doesn't exist
-    echo "Starting rviz session"
-    tmux new-session -s rviz2 -d
+    echo "Starting navigation session"
+    tmux new-session -s navigation -d
     else
     # kill the old sessions and restart a new one
-    kill_rviz
-    echo "killed old rviz2 session"
-    tmux new-session -s rviz2 -d
-    # start the rviz2 for the simulator
+    echo "killed old navigation session"
+    kill_all_tmux
+    tmux new-session -s navigation -d
 fi
-rviz_file="$mount_path/University/Lincoln/MSc/Robot-Programming/CMP9767_LIMO/limo_ros/src/limo_gazebosim/rviz/urdf.rviz"
-# rviz command
-rviz_cmd="rviz2 -d $rviz_file"
-tmux send-keys -t rviz2 "$rviz_cmd" Enter
+#source limo in the session
+tmux send-keys -t navigation "$source_limo_cmd" Enter
+navigation_launch_cmd="ros2 launch limo_navigation limo_navigation.launch.py map:=/media/robert/Portable-Drive/University/Lincoln/MSc/Robot-Programming/CMP9767_LIMO/limo_ros/src/limo_navigation/maps/assessment_map.yaml"
+tmux send-keys -t navigation "$navigation_launch_cmd" Enter
